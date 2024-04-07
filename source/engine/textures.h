e2e5653e6a5569ad219e15bb0c3baa94cd940420
ricmaria <156622743+ricmaria@users.noreply.github.com>
2024-04-07 19:14:21 +0200

    Added shaders and assets. Updated code to support meshes and textures.

---
+++ source/engine/textures.h
@@ -0,0 +1,13 @@
+﻿// vulkan_guide.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <types.h>
+#include <engine.h>
+
+namespace vkutil {
+
+	bool load_image_from_file(VulkanEngine& engine, const char* file, AllocatedImage& outImage);
+
+}
