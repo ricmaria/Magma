f5936931ec50f36cae43f14856bbc3bc565e38f4
ricmaria <156622743+ricmaria@users.noreply.github.com>
2024-04-07 13:16:15 +0200

    Initial commit

---
+++ source/engine/initializers.cpp
@@ -0,0 +1,1 @@
+﻿#include <initializers.h>
---
+++ source/main.cpp
@@ -0,0 +1,14 @@
+#include <engine.h>
+
+int main(int argc, char* argv[])
+{
+	VulkanEngine engine;
+
+	engine.init();	
+	
+	engine.run();	
+
+	engine.cleanup();	
+
+	return 0;
+}
